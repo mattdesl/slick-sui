f61308bc7c8959d19ec52171c7bcce2f4b1318ed
dave.des <dave.des@688cc2e3-bd2b-0410-8374-434c9faad705>
2007-12-08 01:44:44 +0000

    documenting

--- sui/src/mdes/slick/sui/Padding.java
+++ sui/src/mdes/slick/sui/Padding.java
@@ -7,15 +7,34 @@
 package mdes.slick.sui;
 
 /**
- *
+ * The Padding class holds the basic insets for a component. These insets include top, left, right and bottom size.
  * @author davedes
  */
 public class Padding {
     
-    public float top, left, bottom, right;
+    /**
+     * The top spacing.
+     */
+    public float top;
+    /**
+     * The left spacing.
+     */
+    public float left; 
+    /**
+     * The bottom spacing.
+     */
+    public float bottom;
+    /**
+     * The right spacing.
+     */
+    public float right;
     
     /**
-     * Creates a new instance of Padding
+     * Creates a new instance of Padding with the specified spacings.
+     * @param top the top spacing
+     * @param left the left spacing
+     * @param bottom the bottom spacing
+     * @param right the right spacing
      */
     public Padding(float top, float left, float bottom, float right) {
         this.top = top;
@@ -24,14 +43,35 @@ public class Padding {
         this.right = right;
     }
     
+    /**
+     * Creates a new instance of Padding with the specified spacing.
+     * @param num the spacing to apply to all edges
+     */
     public Padding(float num) {
         this(num,num,num,num);
     }
     
+    /**
+     * Creates a new instance of Padding with zero spacing.
+     */
     public Padding() {
         this(0);
     }
     
+    /**
+     * Creates a new instance of Padding from the specified Padding object.
+     * @param pad the Padding object to use for the spacing
+     */
+    public Padding(Padding pad) {
+        this(pad.top, pad.left, pad.bottom, pad.right);
+    }
+    
+    /**
+     * Returns <tt>true</tt> if the given Object is an instance of Padding with the
+     * same spacing as this Padding, otherwise returns <tt>false</tt>.
+     * @param obj the object to check against
+     * @return <tt>true</tt> if the two Padding objects are equal
+     */
     public boolean equals(Object obj) {
         if (obj instanceof Padding) {
             Padding p = (Padding)obj;
@@ -40,10 +80,29 @@ public class Padding {
             return false;
     }
     
+    /**
+     * Creates a new object of the same class as this object. 
+     * @return a clone of this instance. 
+     */
     public Object clone() {
-        return new Padding(top, left, bottom, right);
+        return new Padding(this);
     }
     
+    /**
+     * Sets this Padding to the spacing of the specified Padding object.
+     * @param pad the Padding to use for the new spacings
+     */
+    public void set(Padding pad) {
+        set(pad.top, pad.left, pad.bottom, pad.right);
+    }
+    
+    /**
+     * Sets this Padding to the specified spacings.
+     * @param top the top spacing
+     * @param left the left spacing
+     * @param bottom the bottom spacing
+     * @param right the right spacing
+     */
     public void set(float top, float left, float bottom, float right) {
         this.top = top;
         this.left = left;
@@ -51,10 +110,18 @@ public class Padding {
         this.right = right;
     }
     
+    /**
+     * Sets this Padding to the specified spacing.
+     * @param p the spacing to apply to all edges
+     */
     public void set(float p) {
         set(p, p, p, p);
     }
     
+    /**
+     * Returns a String representation of this Dimension.
+     * @return a String representing this Dimension.
+     */
     public String toString() {
         return super.toString()+"["+top+", "+left+", "+bottom+", "+right+"]";
     }
