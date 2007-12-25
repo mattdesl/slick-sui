4448d5f31c2331ee7d9b64414a260a6906d23904
dave.des <dave.des@688cc2e3-bd2b-0410-8374-434c9faad705>
2007-12-25 18:56:59 +0000

    added @author tag

--- sui/src/mdes/slick/sui/layout/SuiLayout.java
+++ sui/src/mdes/slick/sui/layout/SuiLayout.java
@@ -2,6 +2,10 @@ package mdes.slick.sui.layout;
 
 import mdes.slick.sui.SuiContainer;
 
+/**
+ *
+ * @author Dantarion
+ */
 public interface SuiLayout
 {
 	public void doLayout(SuiContainer container);
