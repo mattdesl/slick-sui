ee37ee52396ff8c67b53604f235c7b10b3614fad
dave.des <dave.des@688cc2e3-bd2b-0410-8374-434c9faad705>
2008-02-15 18:13:36 +0000

    added key repeats for text components, independent of Slick's key repeats

--- sui/src/mdes/slick/sui/TextComponent.java
+++ sui/src/mdes/slick/sui/TextComponent.java
@@ -10,6 +10,7 @@ import mdes.slick.sui.event.*;
 import mdes.slick.sui.skin.ComponentAppearance;
 import mdes.slick.sui.skin.TextComponentAppearance;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.gui.GUIContext;
 
 /**
  * 
@@ -24,6 +25,16 @@ public abstract class TextComponent extends Container {
     protected MouseListener mouseListener = new TextMouseListener();
     private int maxChars = Integer.MAX_VALUE;
     
+    public static final int STANDARD_INITIAL_REPEAT_DELAY = 400;
+    public static final int STANDARD_REPEAT_DELAY = 50;
+    
+    private static boolean defaultRepeatEnabled = true;
+    private static int defaultRepeatDelay = STANDARD_REPEAT_DELAY;
+    private static int defaultInitialRepeatDelay = STANDARD_INITIAL_REPEAT_DELAY;
+    
+    private Timer keyRepeats = new Timer(defaultRepeatDelay);
+    private boolean keyRepeating = defaultRepeatEnabled;
+            
     protected static final String COL_CHAR = "w";
     
     /**
@@ -35,6 +46,8 @@ public abstract class TextComponent extends Container {
         addKeyListener(keyListener);
         addMouseListener(mouseListener);
         setFocusable(true);
+        keyRepeats.setInitialDelay(defaultInitialRepeatDelay);
+        keyRepeats.setRepeats(true);
     }
     
     /**
@@ -134,7 +147,116 @@ public abstract class TextComponent extends Container {
     public void setMaxChars(int maxChars) {
         this.maxChars = maxChars;
     }
-              
+           
+    public void updateComponent(GUIContext ctx, int delta) {
+        super.updateComponent(ctx, delta);
+        
+        if (keyRepeating) {
+            keyRepeats.update(ctx, delta);
+            if (keyRepeats.isAction()) {
+                doRepeat();
+            }
+        }
+    }
+    
+    /**
+     * Provides a hint for the creation of new text components to
+     * enable key repeat with the specified delays. This does not
+     * affect any existing instances of TextComponent. If individual
+     * text component timing is desired, use the instance methods.
+     * 
+     * @param initialDelay the default initial delay for new text components
+     *      to use
+     * @param delay the default delay for new text components to use
+     */
+    public static void enableDefaultKeyRepeat(int initialDelay, int delay) {
+        defaultRepeatEnabled = true;
+        defaultInitialRepeatDelay = initialDelay;
+        defaultRepeatDelay = delay;
+    }
+    
+    /**
+     * Provides a hint for the creation of new text components to
+     * enable key repeat with the last used delays. This does not
+     * affect any existing instances of TextComponent. If individual
+     * text component timing is desired, use the instance methods. If 
+     * no delays were last specified in enableDefaultKeyRepeat, the delays 
+     * will be STANDARD_INITIAL_REPEAT_DELAY and STANDARD_REPEAT_DELAY.
+     */
+    public static void enableDefaultKeyRepeat() {
+        defaultRepeatEnabled = true;
+    }
+    
+    /**
+     * Provides a hint for the creation of new text components to
+     * disable key repeat. This does not affect any existing instances 
+     * of TextComponent. If individual text component timing is desired, 
+     * use the instance methods.
+     */
+    public static void disableDefaultKeyRepeat() {
+        defaultRepeatEnabled = false;
+    }
+    
+    /**
+     * Returns <tt>true</tt> if new text component instances use key repeating.
+     * 
+     * @return <tt>true</tt> if new instances have key repeat enabled, <tt>false</tt> 
+     *      otherwise
+     */
+    public static boolean isDefaultKeyRepeatEnabled() {
+        return defaultRepeatEnabled;
+    }
+    
+    /**
+     * Enables key repeating on this text component instance with the specified delays.
+     * If a "global" or default setting is desired, use the static methods instead. 
+     * <p>
+     * If the key repeats timer is currently running, it will be restarted to reflect the
+     * new delays.
+     *
+     * @param initialDelay the initial delay before repeating keys
+     * @param delay the delay between each key repeat
+     */
+    public void enableKeyRepeat(int initialDelay, int delay) {
+        keyRepeating = true;
+        keyRepeats.setDelay(initialDelay);
+        keyRepeats.setInitialDelay(delay);
+        if (keyRepeats.isRunning())
+            keyRepeats.restart();
+    }
+    
+    /**
+     * Enables key repeating on this text component instance with the last used delays.
+     * If a "global" or default setting is desired, use the static methods instead. If 
+     * no delays were last specified in enableKeyRepeat, the delays will be 
+     * STANDARD_INITIAL_REPEAT_DELAY and STANDARD_REPEAT_DELAY.
+     * <p>
+     * If the key repeats timer is already running, it will <i>not</i> be restarted.
+     */
+    public void enableKeyRepeat() {
+        keyRepeating = true;
+    }
+    
+    /**
+     * Disables key repeating on this text component instance. 
+     * If a "global" or default setting is desired, use the static methods instead.
+     * <p>
+     * If the key repeats timer is currently running, it will be stopped.
+     */
+    public void disableKeyRepeat() {
+        keyRepeating = false;
+        keyRepeats.stop();
+    }
+    
+    /**
+     * Returns <tt>true</tt> if this text component instance has key repeating enabled.
+     * @return <tt>true</tt> if key repeating is enabled on this component, <tt>false</tt>
+     *      otherwise
+     */
+    public boolean isKeyRepeatEnabled() {
+        return keyRepeating;
+    }
+        
     /**
      * Adds the specified listener to the list.
      *
@@ -174,54 +296,75 @@ public abstract class TextComponent extends Container {
         }
     }
     
+    private int key;
+    private char c;
+    
+    private void doRepeat() {
+        if (!isEditable())
+            return;
+
+        if (text == null)
+            text = "";
+        
+        String oldText = text;
+        int oldCaret = caretPos;
+
+        if ( (c<127) && (c>31) && (text.length() < getMaxChars()) ) {
+            if (caretPos < text.length()) {
+                text = text.substring(0, caretPos) + c + text.substring(caretPos);
+            } else {
+                text = text.substring(0, caretPos) + c;
+            }
+            caretPos++;
+        } else if (key == Input.KEY_LEFT) {
+            if (caretPos > 0)
+                caretPos--;
+        } else if (key == Input.KEY_RIGHT) {
+            if (caretPos < text.length())
+                caretPos++;
+        } else if (key == Input.KEY_BACK) {
+            if ((caretPos>0) && (text.length()>0)) {
+                if (caretPos < text.length())
+                    text = text.substring(0, caretPos-1) + text.substring(caretPos);
+                else
+                    text = text.substring(0, caretPos-1);
+                caretPos--;
+            }
+        } else if (key == Input.KEY_DELETE) {
+            if (caretPos < text.length()) {
+                text = text.substring(0, caretPos) + text.substring(caretPos+1);
+            }
+        }
+        doRepeatImpl(key, c);
+
+        if (oldText != text) { //changed
+            textChanged(oldText);
+            fireStateChanged();
+        }
+
+        if (oldCaret != caretPos) {
+            caretPositionChanged(oldCaret);
+        }
+    }
+    
+    protected void doRepeatImpl(int key, char c) {
+        //do nothing
+    }
+    
     protected class TextKeyListener extends KeyAdapter {
         public void keyPressed(KeyEvent e) {
-            if (!isEditable())
-                return;
+            key = e.getKeyCode();
+            c = e.getKeyChar();
+            doRepeat();
             
-            int key = e.getKeyCode();
-            char c = e.getKeyChar();
-            
-            if (text == null)
-                text = "";
-            
-            String oldText = text;
-            int oldCaret = caretPos;
-            
-            if ( (c<127) && (c>31) && (text.length() < getMaxChars()) ) {
-                if (caretPos < text.length()) {
-                    text = text.substring(0, caretPos) + c + text.substring(caretPos);
-                } else {
-                    text = text.substring(0, caretPos) + c;
-                }
-                caretPos++;
-            } else if (key == Input.KEY_LEFT) {
-                if (caretPos > 0)
-                    caretPos--;
-            } else if (key == Input.KEY_RIGHT) {
-                if (caretPos < text.length())
-                    caretPos++;
-            } else if (key == Input.KEY_BACK) {
-                if ((caretPos>0) && (text.length()>0)) {
-                    if (caretPos < text.length())
-                        text = text.substring(0, caretPos-1) + text.substring(caretPos);
-                    else
-                        text = text.substring(0, caretPos-1);
-                    caretPos--;
-                }
-            } else if (key == Input.KEY_DELETE) {
-                if (caretPos < text.length()) {
-                    text = text.substring(0, caretPos) + text.substring(caretPos+1);
-                }
+            if (keyRepeating) {
+                keyRepeats.restart();
             }
-            
-            if (oldText != text) { //changed
-                textChanged(oldText);
-                fireStateChanged();
-            }
-            
-            if (oldCaret != caretPos) {
-                caretPositionChanged(oldCaret);
+        }
+        
+        public void keyReleased(KeyEvent e) {
+            if (e.getKeyCode()==key) {
+                keyRepeats.stop();
             }
         }
     }
